c484078fc62163f7f9c98eba83d57eac9ffbb019
shivansh013 <tshivman90@gmail.com>
2025-01-18 11:52:09 +0530

    Rename app title to 'Recall Riddle'

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'Pages/home_page.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Recall Riddle',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
